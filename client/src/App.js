bee1afeb100c09469a929e2910a9b0c49ebae6d4
yashaleo <yashaleo@me.com>
2019-08-13 12:49:52 -0500

    added components folder and deleted examples js files

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+// import './App.css';
 
-function App() {
+const NavBar = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="navbar-fixed">
+      <nav class="black">
+        <div class="nav-wrapper">
+          <div class="container">
+            <a href="#" class="brand-logo center">
+              Logo
+            </a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li class="active">
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
+};
+
+const ProductCard = props =>{
+  return (
+<div class="col s12 m6">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">{props.productName}</span>
+        </div>
+        <div class="card-action">
+          <a href="#">Buy Product</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+class App extends Component {
+  render() {
+    return <div>
+      <NavBar/>
+      <div className="containter row">
+      <ProductCard productName= "Tent"/>
+      <ProductCard productName= "Pole"/>
+      <ProductCard productName= "Gun"/>
+
+      </div>
+    </div>
+
+  }
 }
 
 export default App;
