697262015fc3d9d7ecb146cf1eebaa5526ad4610
paulgonteacher <50177334+paulgon@users.noreply.github.com>
2019-08-14 13:23:01 -0500

    fixed components

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,9 @@
 import React, { Component } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
-// import './App.css';
+import NavBar from "./components/NavBar";
+import ProductCard from "./components/Products";
 
-const NavBar = () => {
-  return (
-    <div class="navbar-fixed">
-      <nav class="black">
-        <div class="nav-wrapper">
-          <div class="container">
-            <a href="#" class="brand-logo center">
-              Logo
-            </a>
-            <ul id="nav-mobile" class="right hide-on-small-and-down">
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li class="active">
-                <a href="#">About</a>
-              </li>
-              <li>
-                <a href="#">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
-  );
-};
 
-const ProductCard = props =>{
-  return (
-<div class="col s12 m6">
-      <div class="card blue-grey darken-1">
-        <div class="card-content white-text">
-          <span class="card-title">{props.productName}</span>
-        </div>
-        <div class="card-action">
-          <a href="#">Buy Product</a>
-        </div>
-      </div>
-    </div>
-  )
-}
 
 class App extends Component {
   render() {
