caf093c41fd4e6c74bf98b2dc87c35c2ee5f705c
paulgonteacher <50177334+paulgon@users.noreply.github.com>
2019-08-15 13:32:30 -0500

    added background image

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,22 @@ import React, { Component } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import NavBar from "./components/NavBar";
 import ProductCard from "./components/Products";
-
+import VideoHeader from './components/videoHeader';
+import Image from './rainforest.jpg';
 
 
 class App extends Component {
   render() {
     return <div>
       <NavBar/>
+      <video autoPlay loop id="bgvid" src="/assets/video/camping.mp4" type="video/mp4" />
+{/* 
       <div className="containter row">
       <ProductCard productName= "Tent"/>
       <ProductCard productName= "Pole"/>
       <ProductCard productName= "Gun"/>
 
-      </div>
+      </div> */}
     </div>
 
   }
---
+++ client/src/components/videoHeader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const VideoHeader = props => {
+    return (
+        <video autoplay loop id="bgvid">
+                <source src="/assets/video/camping.mp4" type="video/mp4"/>
+        </video>
+            )
+}
+ 
+export default VideoHeader;
