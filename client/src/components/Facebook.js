717d75098dda4aa1a104e71074a949f9d478448e
HH964 <49995995+HH964@users.noreply.github.com>
2019-08-20 14:57:56 -0500

    facebook database works

--- client/src/components/Facebook.js
+++ client/src/components/Facebook.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import FacebookLogin from 'react-facebook-login';
+import axios from "axios"
 
 export default class Facebook extends Component {
   state = {
@@ -10,9 +11,23 @@ export default class Facebook extends Component {
     picture: ''
   };
 
-  responseFacebook = response => {
-    // console.log(response);
+  componentDidUpdate() {
+    console.log("this is firing")
+    const user = {
+      name: this.state.name,
+      facebookID: this.state.userID
+    }
+    
+      axios.post("/api/user/", { user }).then(res => {
+        console.log(res.data);
+      })
+    
+  }
 
+  responseFacebook = response => {
+    console.log(response);
+
+    if (response.status === "unknown") return;
     this.setState({
       isLoggedIn: true,
       userID: response.userID,
@@ -20,6 +35,16 @@ export default class Facebook extends Component {
       email: response.email,
       picture: response.picture.data.url
     });
+    console.log(this.state);
+    const user = {
+      name: this.state.name,
+      facebookID: this.state.userID
+    }
+    
+      axios.post("/api/user/", { user }).then(res => {
+        console.log(res.data);
+      })
+
   };
 
   componentClicked = () => console.log('clicked');
